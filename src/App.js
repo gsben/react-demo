c312a2b1cac669fc832af49bf7150158ca13b16c
p_staoguo <704684827@qq.com>
2023-10-23 21:08:02 +0800

    feat: add text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" style={{borderRadius: "50%"}}/>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ borderRadius: "50%" }}
+        />
         <p>
-          Edi111t <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React start
         </a>
       </header>
     </div>
